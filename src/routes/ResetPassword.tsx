f7c2645e99c1facd525f8162c31c959a121be441
Keegan Williams <KeeganW@users.noreply.github.com>
2023-10-22 21:56:25 -0700

    Simple reset password form fix.

--- src/routes/ResetPassword.tsx
+++ src/routes/ResetPassword.tsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
-import { useForm } from 'react-hook-form'
 import axios from 'src/axiosAuth'
 import { Navigate, useParams } from 'react-router-dom'
 import { CenteredPage } from 'src/components/CenteredPage'
-import { Title } from '@mantine/core'
+import { PasswordInput, TextInput, Title } from '@mantine/core'
 import { SMALL_WIDTH } from 'src/utils/helpers'
+import { useForm } from '@mantine/form'
 
 export const ResetPassword: React.FC = () => {
-  const { register, handleSubmit } = useForm()
   const [passwordResetSent, setPasswordResetSent] = useState<boolean>(false)
 
   const params = useParams()
   const { username, token } = params
   const usernameSafe = username || ''
   const tokenSafe = token || ''
+
+  const initialValues = {
+    initialValues: {
+      username: usernameSafe,
+      reset_token: tokenSafe !== '' ? tokenSafe : undefined,
+    },
+  }
+  const form = useForm(initialValues)
+  useEffect(() => {
+    form.setValues(initialValues.initialValues)
+  }, [params])
 
   const handleOnSubmit = (data: any) => {
     axios
@@ -38,38 +48,33 @@ export const ResetPassword: React.FC = () => {
     }
     return (
       <CenteredPage pageWidth={SMALL_WIDTH}>
-        <Form onSubmit={handleSubmit(handleOnSubmit)}>
-          <Form.Group className="mb-3" controlId="formHidden">
-            <Form.Control
-              type="hidden"
-              {...register('username', { value: usernameSafe })}
-            />
-          </Form.Group>
+        <Form onSubmit={form.onSubmit((values: any) => handleOnSubmit(values))}>
+          <TextInput
+            id="username"
+            // Hiding this breaks mantine form things, so don't actually hide it, use this hack
+            style={{ visibility: 'hidden', position: 'absolute' }}
+            {...form.getInputProps('username')}
+          />
+          <TextInput
+            id="reset_token"
+            // Hiding this breaks mantine form things, so don't actually hide it, use this hack
+            style={{ visibility: 'hidden', position: 'absolute' }}
+            {...form.getInputProps('reset_token')}
+          />
 
-          <Form.Group className="mb-3" controlId="formHidden">
-            <Form.Control
-              type="hidden"
-              {...register('reset_token', { value: tokenSafe })}
-            />
-          </Form.Group>
+          <PasswordInput
+            id="new_password"
+            label="New Password"
+            placeholder="New Password"
+            {...form.getInputProps('new_password')}
+          />
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>New Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="New Password"
-              {...register('new_password', { required: true })}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Confirm Password"
-              {...register('confirm_password', { required: true })}
-            />
-          </Form.Group>
+          <PasswordInput
+            id="confirm_password"
+            label="Confirm Password"
+            placeholder="Confirm Password"
+            {...form.getInputProps('confirm_password')}
+          />
           <Button variant="primary" type="submit">
             Submit
           </Button>
@@ -86,16 +91,12 @@ export const ResetPassword: React.FC = () => {
   }
   return (
     <CenteredPage pageWidth={SMALL_WIDTH}>
-      <Form onSubmit={handleSubmit(handleOnSubmit)}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="username"
-            placeholder="Enter username"
-            {...register('username', { required: true })}
-          />
-          <Form.Text className="text-muted" />
-        </Form.Group>
+      <Form onSubmit={form.onSubmit((values: any) => handleOnSubmit(values))}>
+        <TextInput
+          id="username"
+          label="Username"
+          {...form.getInputProps('username')}
+        />
         <Button variant="primary" type="submit">
           Submit
         </Button>
